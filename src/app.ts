17376b1f7270521aaabcc9340e65f44c12c7c6ad
Masayuki-Suzuki <m.suzuki.fp@gmail.com>
2020-12-18 07:54:13 -0800

    feat: All done ✨

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,21 @@ import express from 'express'
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In middle ware!')
+app.use('/', (req, res, next) => {
+    console.log('First middleware')
     next()
 })
 
-app.use((req, res) => {
-    res.send('<h1 style="font-family: sans-serif;">Hello World!</h1>')
+app.use('/users', (req, res) => {
+    console.log('Second middleware')
+    res.send('<h2 style="font-family: sans-serif; font-weight: 500">Users</h2>')
+})
+
+app.use('/', (req, res) => {
+    console.log('3rd middleware')
+    res.send(
+        '<h2 style="font-family: sans-serif; font-weight: 500">Hello!</h2>'
+    )
 })
 
 app.listen(4000)
