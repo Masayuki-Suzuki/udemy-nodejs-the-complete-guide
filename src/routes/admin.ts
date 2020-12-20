9605d912acab51a007a85c6210d1158976d28791
Masayuki-Suzuki <m.suzuki.fp@gmail.com>
2020-12-19 23:03:54 -0800

    feat: Working on to convert html to pug ✨

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,30 +2,50 @@ import path from 'path'
 import express from 'express'
 import { rootPath } from '../utils/rootPath'
 
+type Product = {
+    title: string
+}
+
+type RequestAddProduct = Request
+
 const router = express.Router()
 const getFilePath = (filename: string): string => {
     return path.join(rootPath, 'views', 'admin', `${filename}.html`)
 }
+
+const products: Product[] = []
 
 router.get('/add-product', (req, res) => {
     res.sendFile(getFilePath('add-product'))
 })
 
 router.get('/products', (req, res) => {
-    res.sendFile(getFilePath('products'))
+    res.render('./admin/dashboard', {
+        title: 'Products list',
+        path: 'products',
+        pageTitle: 'Products'
+    })
 })
 
 router.get('/dashboard', (req, res) => {
-    res.sendFile(getFilePath('dashboard'))
+    res.render('./admin/dashboard', {
+        title: 'Admin Dashboard',
+        path: 'dashboard',
+        pageTitle: 'Dashboard'
+    })
 })
 
 router.get('/', (req, res) => {
     res.redirect('/admin/dashboard')
 })
 
-router.post('/product', (req, res) => {
-    console.log(req.body)
-    res.redirect('/')
-})
+router.post(
+    '/product',
+    (req: express.Request<unknown, unknown, Product>, res) => {
+        console.log(req.body.title)
+        products.push({ title: req.body.title })
+        res.redirect('/')
+    }
+)
 
 export default router
